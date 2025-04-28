26cc02b9f81269785d343509c73774432be13a18
jacob_he <148776941+jacobhqh1997@users.noreply.github.com>
2025-04-28 09:29:38 +0800

    Update cut_heatmap.py

--- data_prepare/cut_heatmap.py
+++ data_prepare/cut_heatmap.py
@@ -4,38 +4,31 @@ import os
 import multiprocessing
 from concurrent.futures import ProcessPoolExecutor
 
-
 def cut_empty(matrix_path, save_base_dir):
     matrix = np.load(matrix_path)
-    empty = matrix[:,:,1]
-    coor = np.where(empty<1)
-    top = min(coor[0])
-    bottom = max(coor[0])
-    left = min(coor[1])
-    right = max(coor[1])
-    matrix = matrix[top:bottom,left:right]
-
+    save_path = os.path.join(save_base_dir, '{}.npy'.format(os.path.basename(matrix_path)[:-4]))
+    if os.path.exists(save_path):
+        print('{} already exists'.format(save_path))
+        return
     height, width, _ = matrix.shape
     new_size = 0
 
     if height >= width:
         new_size = height+8
-        new_matrix = np.zeros((new_size, new_size,8))
-        new_matrix[:,:,1] = 1
+        new_matrix = np.zeros((new_size, new_size,16))
+ 
         dealt_width = int((height-width)/2)
         new_matrix[4:-4, dealt_width+4:width+(dealt_width+4)] = matrix
     else:
         new_size = width+8
-        new_matrix = np.zeros((new_size, new_size,8))
-        new_matrix[:,:,1] = 1
+        new_matrix = np.zeros((new_size, new_size,16))
+
         dealt_height = int((width-height)/2)
         new_matrix[dealt_height+4:height+(dealt_height+4),4:-4] = matrix
 
-
-
-    save_path = os.path.join(save_base_dir, '{}.npy'.format(os.path.basename(matrix_path)[:-4]))
     save_dir = os.path.dirname(save_path)
     os.makedirs(save_dir, exist_ok=True)
+    
     np.save(save_path, new_matrix)
 
 
