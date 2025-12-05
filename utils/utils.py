fb87785a5e4c6784e0dd5c06e30f8656e98ea265
jacob_he <148776941+jacobhqh1997@users.noreply.github.com>
2025-12-05 11:36:32 +0800

    Create utils.py

---
+++ utils/utils.py
@@ -0,0 +1,10 @@
+import argparse
+
+def get_args():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument(
+        '-c', '--config',
+        default='None',
+        help='The Configuration file')
+    args = argparser.parse_args()
+    return args
